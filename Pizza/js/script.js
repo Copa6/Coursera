5c8cd78a971f21ea6a8ffa806ef7c4966da506c8
Saurabh Singh <Saurabh Singh>
2016-07-16 22:26:59 +0530

    body top

--- Pizza/js/script.js
+++ Pizza/js/script.js
@@ -1,17 +1,20 @@
 (function(){
 	var move = $('body');
 	var main = $('.main');
+	var head= $('.clickbutton')
 	$('.navigator').bind('click', function(){
 		move.toggleClass('slide-body');
 		main.toggleClass('col-xs-9 col-sm-11 col-md-11');
 		main.removeClass('col-xs-12');
+		head.toggleClass('mybutton');
 		return false;
 		
 	});
 	$('.navigator').bind('blur', function(){
-		move.toggleClass('slide-body');
+		move.removeClass('slide-body');
 		main.addClass('col-xs-12');
 		main.removeClass('col-xs-9 col-sm-11 col-md-11');
+		head.removeClass('mybutton');
 	});
 
 
