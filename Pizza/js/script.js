73bbacc797c13a7f83e3bc8ce776d8e2e10b3878
Saurabh Singh <Saurabh Singh>
2016-07-16 21:25:22 +0530

    moving container

--- Pizza/js/script.js
+++ Pizza/js/script.js
@@ -1,10 +1,28 @@
 (function(){
 	var move = $('body');
+	var main = $('.main');
 	$('.navigator').bind('click', function(){
 		move.toggleClass('slide-body');
+		main.toggleClass('col-xs-9 col-sm-11 col-md-11');
+		main.removeClass('col-xs-12');
 		return false;
+		
 	});
 	$('.navigator').bind('blur', function(){
-		move.toggleClass('slide-body')
+		move.toggleClass('slide-body');
+		main.addClass('col-xs-12');
+		main.removeClass('col-xs-9 col-sm-11 col-md-11');
 	});
+
+
+
+	  /* document.querySelector("body")
+      .addEventListener("mousemove",
+        function (event) {
+          if (event.shiftKey === true) {
+            console.log("x: " + event.clientX);
+            console.log("y: " + event.clientY);
+          }
+        }
+      );*/
 })();
