c53246f697e97f660265f961537d36c15ab6091a
Saurabh Singh <Saurabh Singh>
2016-07-16 22:38:52 +0530

    moving container

--- Pizza/js/script.js
+++ Pizza/js/script.js
@@ -1,12 +1,10 @@
 (function(){
 	var move = $('body');
 	var main = $('.main');
-	var head= $('.clickbutton')
 	$('.navigator').bind('click', function(){
 		move.toggleClass('slide-body');
 		main.toggleClass('col-xs-9 col-sm-11 col-md-11');
 		main.removeClass('col-xs-12');
-		head.toggleClass('mybutton');
 		return false;
 		
 	});
@@ -14,7 +12,6 @@
 		move.removeClass('slide-body');
 		main.addClass('col-xs-12');
 		main.removeClass('col-xs-9 col-sm-11 col-md-11');
-		head.removeClass('mybutton');
 	});
 
 
