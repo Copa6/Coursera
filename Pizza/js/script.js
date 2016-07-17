ba34a820435970a3e4c514d7d92037e4e1ceb92e
Saurabh Singh <Saurabh Singh>
2016-07-17 09:55:33 +0530

    button changes to cross

--- Pizza/js/script.js
+++ Pizza/js/script.js
@@ -1,10 +1,13 @@
 (function(){
 	var move = $('.content');
 	var main = $('.main');
+	var clicker= $('.clickbutton')
 	$('.navigator').bind('mouseenter', function(){
 		move.toggleClass('slide-body');
 		main.toggleClass('col-xs-9 col-sm-11 col-md-11');
 		main.toggleClass('col-xs-12');
+		clicker.addClass('glyphicon-remove');
+		clicker.toggleClass('glyphicon-align-justify');
 		return false;
 		
 	});
@@ -12,11 +15,15 @@
 		move.removeClass('slide-body');
 		main.addClass('col-xs-12');
 		main.removeClass('col-xs-9 col-sm-11 col-md-11');
+		clicker.removeClass('glyphicon-remove');
+		clicker.addClass('glyphicon-align-justify');
 	});
 	$('.content').bind('click', function(){
 		move.removeClass('slide-body');
 		main.addClass('col-xs-12');
 		main.removeClass('col-xs-9 col-sm-11 col-md-11');
+		clicker.removeClass('glyphicon-remove');
+		clicker.addClass('glyphicon-align-justify');
 	});
 
 
