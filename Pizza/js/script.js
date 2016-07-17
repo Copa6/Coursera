93316748d6a504f62c8fc3e17ed7301845b9bc07
Saurabh Singh <Saurabh Singh>
2016-07-17 10:23:16 +0530

    button changes only on click

--- Pizza/js/script.js
+++ Pizza/js/script.js
@@ -1,17 +1,8 @@
 (function(){
 	var move = $('.content');
 	var main = $('.main');
-	var clicker= $('.clickbutton')
-	$('.navigator').bind('mouseenter', function(){
-		move.toggleClass('slide-body');
-		main.toggleClass('col-xs-9 col-sm-11 col-md-11');
-		main.toggleClass('col-xs-12');
-		clicker.toggleClass('glyphicon-remove');
-		clicker.toggleClass('glyphicon-align-justify');
-		return false;
-		
-	});
-	if ($(window).width() <= 800){	
+	var clicker= $('.clickbutton');
+
 	$('.navigator').bind('click', function(){
 		move.toggleClass('slide-body');
 		main.toggleClass('col-xs-9 col-sm-11 col-md-11');
@@ -21,14 +12,14 @@
 		return false;
 		
 	});
-}
-	$('.navigator').bind('mouseleave', function(){
+
+	/*$('.navigator').bind('mouseleave', function(){
 		move.removeClass('slide-body');
 		main.addClass('col-xs-12');
 		main.removeClass('col-xs-9 col-sm-11 col-md-11');
 		clicker.removeClass('glyphicon-remove');
 		clicker.addClass('glyphicon-align-justify');
-	});
+	});*/
 	$('.content').bind('click', function(){
 		move.removeClass('slide-body');
 		main.addClass('col-xs-12');
